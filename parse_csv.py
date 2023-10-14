2bdca9bd7d9388c5af44cf1da399d94acc0b8a2e
Susan Wu <susannmwu@gmail.com>
2023-10-13 17:19:47 -0700

    cleaned pickleball.csv file

--- parse_csv.py
+++ parse_csv.py
@@ -1,14 +1,25 @@
 import csv
 import pandas as pd
+from datetime import datetime
+import numpy as py
 import matplotlib.pyplot as plt
+
 
 # df.columns = Index(['sep=', 'Unnamed: 1'], dtype='object')
 # Read the .csv file
-data = pd.read_csv("pb.csv")
 
-col = data.columns
-print(col[2])
+with open("pickleball.csv", "r") as source:
+    reader = csv.reader(source)
+    # for line in reader:
+    #     print(line[4])
 
+    for line in reader:
+        original_date = line[2]
+
+        duration = line[6]
+        # print(f"duration: {duration} min")
+
+########################################################
 # Printing type of arr object
 # print("Array is of type: ", type(data))
 
